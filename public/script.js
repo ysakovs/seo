bf56b7a97e2c42f02cd91c71a65b34d22cf13818
ysakovs <ivo.isakovs@gmail.com>
2021-02-20 22:06:23 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // connect to main namespace
-const socket = io.connect( 'https://seo-apox4.ondigitalocean.app/', {
+const socket = io.connect( '/', {
     transports: ['websocket'],
     upgrade: false,
     reconnection: true,
